bccb68c130274da4f728377f4452f94e3a19fd57
rqh656418510 <656418510@qq.com>
2022-06-17 20:53:36 +0800

    feat: update configs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,8 @@
 import { navbar, sidebar } from './configs'
 const { searchPlugin } = require('@vuepress/plugin-search')
 const { defaultTheme } = require('@vuepress/theme-default')
+const { nprogressPlugin } = require('@vuepress/plugin-nprogress')
+const { readmorePlugin } = require('vuepress-plugin-popular-dev2')
 
 module.exports = {
 
@@ -44,6 +46,30 @@ module.exports = {
           placeholder: '',
         }
       }
+    }),
+    readmorePlugin({
+      // 已申请的博客 ID
+      blogId: '75474-8185509348394-212',
+      // 已申请的微信公众号名称
+      name: '全栈技术驿站',
+      // 已申请的微信公众号回复关键词
+      keyword: 'Tech',
+      // 已申请的微信公众号二维码链接
+      qrcode: 'https://www.techgrow.cn/img/wx_mp_qr.png',
+      // 文章内容的 JS 选择器，若使用的不是官方默认主题，则需要根据第三方的主题来设置
+      selector: 'div.theme-default-content',
+      // 自定义的 JS 资源链接，可用于 CDN 加速
+      libUrl: 'https://qiniu.techgrow.cn/readmore/dist/readmore.js',
+      // 自定义的 CSS 资源链接，可用于适配不同风格的博客
+      cssUrl: 'https://docs.techgrow.cn/v1/css/vuepress.css',
+      // 文章排除添加引流工具的 URL 规则，支持使用路径、通配符、正则表达式的匹配规则
+      excludes: { strExp: [], regExp: ['^(?!\/v2/demo).*'] },
+      // 是否反转 URL 排除规则的配置，即只有符合排除规则的文章才会添加引流工具
+      reverse: false,
+      // 文章解锁后凭证的有效天数
+      expires: 365,
+      // 每篇文章随机添加引流工具的概率，有效范围在 0.1 ~ 1 之间，1 则表示所有文章默认都自动添加引流工具
+      random: 1
     })
-  ],
+  ]
 }
