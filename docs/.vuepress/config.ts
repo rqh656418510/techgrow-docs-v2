0a67c1ef74f27e9c81ca4fadf88e83629dafc9ef
rqh656418510 <656418510@qq.com>
2022-06-18 10:33:50 +0800

    feat: update configs and pages

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -60,7 +60,7 @@ module.exports = {
       // 自定义的 JS 资源链接，可用于 CDN 加速
       libUrl: 'https://qiniu.techgrow.cn/readmore/dist/readmore.js',
       // 自定义的 CSS 资源链接，可用于适配不同风格的博客
-      cssUrl: 'https://qiniu.techgrow.cn/readmore/dist/vuepress2.css',
+      cssUrl: 'https://docs.techgrow.cn/v2/css/vuepress2.css',
       // 文章排除添加引流工具的 URL 规则，支持使用路径、通配符、正则表达式的匹配规则
       excludes: { strExp: [], regExp: ['^(?!\/v2/demo).*'] },
       // 是否反转 URL 排除规则的配置，即只有符合排除规则的文章才会添加引流工具
