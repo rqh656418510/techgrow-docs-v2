3dd5ea97b34d63f928ef92e44969e8fd738af1a7
rqh656418510 <656418510@qq.com>
2024-03-15 13:29:34 +0800

    feat: update pages

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -92,10 +92,10 @@ module.exports = {
       excludes: { strExp: [], regExp: ['^(?!\/v2/wechat/demo).*'] },
       // 是否反转 URL 排除规则的配置，即只有符合排除规则的文章才会添加引流工具
       reverse: false,
+      // 文章内容的预览高度(例如 300)，设置值为 auto 表示预览高度自适应
+      height: 'auto',
       // 移动端的页面是否添加微信公众号引流工具
       allowMobile: true,
-      // 文章内容的预览高度(例如 300)
-      height: 'auto',
       // 文章解锁后凭证的有效天数
       expires: 365,
       // 定时校验凭证有效性的时间间隔（秒）
